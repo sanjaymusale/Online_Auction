c9593236f71fe2e4d57ce309098ba7bfbb323f6f
nischal <nischalcreative@gmail.com>
2019-03-26 23:22:35 +0530

    int

--- app/controllers/products_controller.js
+++ app/controllers/products_controller.js
@@ -31,7 +31,7 @@ router.post('/', authenticateUser, upload.array('myimage', 2), (req, res) => {
     })
     console.log(image)
     body.imageUrl = image
-    const output = "asads"
+    const output = "hello"
     const product = new Product(body)
     product.save()
         .then((product) => {
