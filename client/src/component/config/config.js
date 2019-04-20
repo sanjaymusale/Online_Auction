e711b53313c9ddf17eeee95fbe2b3b8cf37479e4
sanjaymusale.10@gmail.com <sanjaymusale.10@gmail.com>
2019-04-20 16:09:46 +0530

    set uri

--- client/src/component/config/config.js
+++ client/src/component/config/config.js
@@ -1,5 +1,7 @@
 const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3001'
-
+console.log(process.env)
+console.log(process.env.SERVER_URL)
+console.log(process.env.MONGODB_URI)
 module.exports = {
     SERVER_URL
 }
