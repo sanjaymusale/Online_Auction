50e00a8bc9c6f11491e91d4b821d0a3aa384f369
Sanjay Musale <sanjay@codebrahma.com>
2019-12-20 16:15:05 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,7 +4,6 @@ mongoose.Promise = global.Promise
 const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/onlinebidding'
 
 //'mongodb+srv://<username>:<password>@cluster0-z3l82.mongodb.net/contact-manager?retryWrites=true'
-//mongodb+srv://Sanjaynm69:Sanjaynm69@cluster0-1rctg.mongodb.net/online-aucyion?retryWrites=true
 console.log('connectted',CONNECTION_URI)
 mongoose.connect(CONNECTION_URI, { useNewUrlParser: true })
     .then(() => {
